eab7c910364f289d4054464e625e97046ddf2233
PolinaScetkovska <45237211+PolinaScetkovska@users.noreply.github.com>
2020-05-22 22:38:39 +0200

    Update main.js
    make the box to move on Y axis

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,10 +1,12 @@
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.01;
+var rotationSpeedy = 0.03;
 var myOtherBox = document.getElementById('myOtherBox');
 
 
 function spin(){
   myOtherBox.object3D.rotation.x -= rotationSpeed;
+  myOtherBox.object3D.rotation.y += rotationSpeedy;
   console.log(myOtherBox.object3D.rotation.x);
 }
 
